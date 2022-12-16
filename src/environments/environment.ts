2b1887b351b3fa3504979a04b778ef99b614d90e
limnemuel22 <lim.nemuel.22@gmail.com>
2022-12-16 19:46:06 +0800

    remove firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,6 +7,7 @@ export const environment = {
     projectId: 'store-exam',
     appId: '1:878491166002:web:5b52d8f5de319a15c6a32b',
     storageBucket: 'store-exam.appspot.com',
+    locationId: 'us-central',
     apiKey: 'AIzaSyB-mBuCUTKoZPvlLTdEnwTTaPAS52C6bhk',
     authDomain: 'store-exam.firebaseapp.com',
     messagingSenderId: '878491166002',
